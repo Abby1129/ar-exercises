2b1a0f4538810a851e8611fbcf00a281fb94a341
Abby <abby_faigao@yahoo.com>
2022-04-07 18:24:10 +0000

    Add error validations to models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,26 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+# Ask the user for a store name (store it in a variable)
+
+puts "Please enter a store name:"
+store_name = gets.chomp
+
+
+# # Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+
+new_store = Store.new(name:store_name)
+
+# # Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+
+new_store.save
+
+new_store.errors.full_messages.each do |message|
+  puts message
+end
+
+
+
+
+
+
--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,20 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0}
+  validate :apparel_validation
+
+  def apparel_validation
+    if mens_apparel.blank? && womens_apparel.blank?
+      errors.add(:mens_apparel, "can't be blank")
+      errors.add(:womens_apparel, "can't be blank")
+    end
+  end
 
 end
+
+# Stores must always have a name that is a minimum of 3 characters
+# Stores have an annual_revenue that is a number (integer) that must be 0 or more
+# BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a custom validation method - don't use a Validator class)
